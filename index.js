3f5dc0e305fedca1d994e232424e285340aa7cc4
rohitpratapitm <rohitpratapitm@gmail.com>
2022-02-14 23:57:04 -0500

    Added random dob

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 // run `node index.js` in the terminal
 let fs = require('fs');
 let csv = require('fast-csv');
+let moment = require('moment');
 
 var stream = fs.createReadStream('us-500.csv');
-let dob = "'1978-06-07'";
 csv
   .parseStream(stream, { headers: true })
   .on('data', function (data) {
+    const dobString = randomDate(new Date(1950, 0, 1), new Date(2012, 0, 1));
+    const dob = moment(dobString).format('yyyy-MM-DD');
     console.log(
-      `insert into customer values (seq_customer.nextval, ${data.firstname}, ${data.lastname}, ${data.email}, DATE ${dob},${data.phone1}, 'amJ1dHRAZ21haWwuY29t');`
+      `insert into customer values (seq_customer.nextval, ${data.firstname}, ${data.lastname}, ${data.email}, DATE '${dob}',${data.phone1}, 'amJ1dHRAZ21haWwuY29t');`
     );
   })
   .on('end', function () {
     console.log('done');
   });
+
+function randomDate(start, end) {
+  return new Date(
+    start.getTime() + Math.random() * (end.getTime() - start.getTime())
+  );
+}
+
 console.log(`Hello Node.js v${process.versions.node}!`);
