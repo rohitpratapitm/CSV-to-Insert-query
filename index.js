619d53ffdec0d7f822c64bec188e84dc63035e65
rohitpratapitm <rohitpratapitm@gmail.com>
2022-02-15 23:00:10 -0500

    Schedule inserts added.

--- index.js
+++ index.js
@@ -3,26 +3,78 @@ let fs = require('fs');
 let csv = require('fast-csv');
 let moment = require('moment');
 
-var stream = fs.createReadStream('us-500.csv');
-var i = 0;
-csv
-  .parseStream(stream, { headers: true })
-  .on('data', function (data) {
-    if (i >= 480 && i <= 500) {
-      // if (i >= 0 && i <= 20) {
-      // console.log(generateCustomer(data));
-      console.log(generateStaff(data));
-    }
-    i++;
-  })
-  .on('end', function () {
-    console.log('done');
-  });
+function readExcel(name) {
+  var stream = fs.createReadStream(name);
+  csv
+    .parseStream(stream, { headers: true })
+    .on('data', function (data) {
+      console.log(generateMenu(data));
+    })
+    .on('end', function () {
+      console.log('done');
+    });
+}
 
 function generateCustomer(data) {
   const dob = getDob();
   const password = generatePassword();
   return `insert into customer values (seq_customer.nextval, '${data.firstname}', '${data.lastname}', '${data.email}', DATE '${dob}',${data.phone1}, '${password}');`;
+}
+
+function generateInventory(data) {
+  return `insert into inventory values (seq_inventory.nextval, '${data.itemname}', '${data.category}', ${data.quantity});`;
+}
+
+function generateDiningTable() {
+  const status = 'Empty';
+  const capacity = Math.floor(Math.random() * (10 - 2) + 2);
+  const randomIndex = Math.floor(Math.random() * (3 - 0) + 0);
+  const locations = ['Corner', 'Center', 'Outside'];
+  const location = locations[randomIndex];
+  return `insert into diningtable values (seq_diningtable.nextval, '${status}', ${capacity}, '${location}');`;
+}
+
+function generateStaffSchedule(staffId, month, year, shift) {
+  const daysInAMonth = daysInMonth(month, year);
+  for (var i = 1; i <= daysInAMonth; i++) {
+    if (i % 7 === 0) {
+      // Sunday is a holiday
+      continue;
+    }
+    const date = i < 10 ? `0${i}` : i;
+    console.log(
+      `insert into schedule values (${staffId}, DATE '${year}-${month}-${date}', TIMESTAMP '${year}-${month}-${date} ${shift.startTime}', TIMESTAMP '${year}-${month}-${date} ${shift.endTime}');`
+    );
+  }
+}
+
+function generateScheduleForStaff() {
+  const shifts = ['morning', 'afternoon', 'evening'];
+  const shiftTiming = new Map();
+  shiftTiming.set(shifts[0], { startTime: '06:00:00', endTime: '12:00:00' });
+  shiftTiming.set(shifts[1], { startTime: '12:00:00', endTime: '18:00:00' });
+  shiftTiming.set(shifts[2], { startTime: '18:00:00', endTime: '00:00:00' });
+
+  for (var staffId = 1; staffId <= 6; staffId++) {
+    generateStaffSchedule(staffId, 02, 2022, shiftTiming.get('morning'));
+  }
+
+  for (var staffId = 7; staffId <= 12; staffId++) {
+    generateStaffSchedule(staffId, 02, 2022, shiftTiming.get('afternoon'));
+  }
+
+  for (var staffId = 13; staffId <= 20; staffId++) {
+    generateStaffSchedule(staffId, 02, 2022, shiftTiming.get('evening'));
+  }
+}
+
+function daysInMonth(month, year) {
+  return new Date(year, month, 0).getDate();
+}
+
+function generateMenu(data) {
+  const price = Math.floor(Math.random() * (100 - 10) + 10);
+  return `insert into menu values (seq_menu.nextval, '${data.itemname}', '${data.category}', ${price});`;
 }
 
 function getDob() {
@@ -65,3 +117,9 @@ function generatePassword() {
 }
 
 console.log(`Hello Node.js v${process.versions.node}!`);
+// readExcel('inventory.csv');
+// for (var i = 0; i < 20; i++) {
+//   console.log(generateDiningTable());
+// }
+
+generateScheduleForStaff();
